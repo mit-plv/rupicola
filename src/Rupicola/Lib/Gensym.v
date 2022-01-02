f66d1c8af123fe4d8424a84ca41a825483f71425
Clément Pit-Claudel <clement.pitclaudel@live.com>
2022-01-02 01:11:56 -0500

    lib/gensym: Make prefix configurable

--- src/Rupicola/Lib/Gensym.v
+++ src/Rupicola/Lib/Gensym.v
@@ -1,8 +1,12 @@
 From Coq Require Import
      String Numbers.DecimalString.
 
-Definition _gs (prefix: string) (n: nat) :=
-  ("_gs_" ++ prefix ++ NilEmpty.string_of_uint (Nat.to_uint n))%string.
+Definition _gs (gs_prefix prefix: string) (n: nat) :=
+  (gs_prefix ++ prefix ++
+   match n with
+   | 0 => ""
+   | S n => NilEmpty.string_of_uint (Nat.to_uint n)
+   end)%string.
 
 Definition gs {prefix: string} {n: nat} (str: string) :=
   str.
@@ -13,13 +17,17 @@ Ltac gensym_next locals prefix n :=
   | _ => n
   end.
 
+Ltac gensym_prefix :=
+  constr:("_gs_"%string).
+
 Ltac gensym locals prefix :=
   let n0 := match locals with
            | context[gs prefix ?n] => constr:(S n)
            | _ => constr:(0%nat)
            end in
   let n := gensym_next locals prefix n0 in
-  let str := constr:(_gs prefix n) in
+  let gs_prefix := gensym_prefix in
+  let str := constr:(_gs gs_prefix prefix n) in
   let str := (eval vm_compute in str) in
   constr:(@gs prefix n str).
 
