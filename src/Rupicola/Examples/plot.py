fe45fe7c86faed115bb920d26895a9cbc1ec8191
Clément Pit-Claudel <clement.pitclaudel@live.com>
2022-03-05 02:41:34 -0800

    bench: Clean up plots

--- src/Rupicola/Examples/plot.py
+++ src/Rupicola/Examples/plot.py
@@ -5,13 +5,13 @@ import pandas, seaborn, matplotlib.pyplot
 
 ALIASES = {
     'benchmark': [
-        ('fnv1a64', 'fnv1a'),
+        ('crc32', 'crc32'),
         ('utf8_decode', 'utf8'),
         ('murmur3', 'm3s'),
         ('upstr', 'upstr'),
         ('ip_checksum', 'ip'),
         ('revcomp', 'fasta'),
-        ('crc32', 'crc32'),
+        ('fnv1a64', 'fnv1a'),
     ],
     'compiler': [
         ("gcc-9.4.0", "GCC 9.4"),
@@ -21,8 +21,7 @@ ALIASES = {
         ("clang-11.0.0", "Clang 11.0"),
         ("clang-12.0.0", "Clang 12.0"),
         ("clang-13.0.1", "Clang 13.0"),
-        ("ocamlopt-4.07.0", "ocamlopt 4.07"),
-        ("ocamlopt-4.09.0", "ocamlopt 4.09")
+        ("ocamlopt-4.09.1", "ocamlopt 4.09")
     ],
     'language': [
         ("rupicola", "Rupicola"),
@@ -35,24 +34,19 @@ ALIASES = {
 }
 
 COLORS = {
-    ('GCC 9.4',       'C'):                      '#5c3566',
-    ('GCC 10.3',      'C'):                      '#75507b',
-    ('GCC 11.1',      'C'):                      '#ad7fa8',
     ('GCC 9.4',       'Rupicola'):               '#8f5902',
     ('GCC 10.3',      'Rupicola'):               '#c17d11',
     ('GCC 11.1',      'Rupicola'):               '#e9b96e',
-
-    ('Clang 11.0',    'C'):                      '#204a87',
-    ('Clang 12.0',    'C'):                      '#3465a4',
-    ('Clang 13.0',    'C'):                      '#729fcf',
     ('Clang 11.0',    'Rupicola'):               '#ce5c00',
     ('Clang 12.0',    'Rupicola'):               '#f57900',
     ('Clang 13.0',    'Rupicola'):               '#fcaf3e',
 
-    ('ocamlopt 4.07', 'Coq/OCaml Impl'):         '#a40000',
-    ('ocamlopt 4.07', 'Coq/OCaml'):              '#cc0000',
-    ('ocamlopt 4.07', 'Coq/OCaml Impl (sound)'): '#ef2929',
-    ('ocamlopt 4.07', 'Coq/OCaml (sound)'):      '#ef2929',
+    ('GCC 9.4',       'C'):                      '#5c3566',
+    ('GCC 10.3',      'C'):                      '#75507b',
+    ('GCC 11.1',      'C'):                      '#ad7fa8',
+    ('Clang 11.0',    'C'):                      '#204a87',
+    ('Clang 12.0',    'C'):                      '#3465a4',
+    ('Clang 13.0',    'C'):                      '#729fcf',
 
     ('ocamlopt 4.09', 'Coq/OCaml Impl'):         '#a40000',
     ('ocamlopt 4.09', 'Coq/OCaml'):              '#cc0000',
@@ -75,10 +69,18 @@ def prepare(data, selected_benchmarks):
 
     return df
 
-def add_plot(data, configs, xlabel, selected_benchmarks=None, ax=None, logmin=False):
+def filter_configs(df, configs):
+    return {cfg: color for (cfg, color) in configs.items()
+            if not df[df['config'] == cfg].empty}
+
+def add_plot(data, colors, xlabel, selected_benchmarks=None, ax=None, logmin=False):
     df = prepare(data, selected_benchmarks)
-    seaborn.set_palette(seaborn.color_palette([COLORS[c] for c in configs]))
-    hue_order = [lang + " " + comp for (comp, lang) in configs]
+
+    configs = {lang + " " + comp: color for ((comp, lang), color) in colors.items()}
+    configs = filter_configs(df, configs)
+
+    hue_order = list(configs.keys())
+    seaborn.set_palette(seaborn.color_palette(list(configs.values())))
 
     if selected_benchmarks is None:
         selected_benchmarks = [k for _, k in ALIASES['benchmark']]
@@ -111,10 +113,8 @@ def c_benchmarks():
         return
     fig = matplotlib.pyplot.figure(figsize=(8, 7))
 
-    CONFIGS = [k for k in COLORS if "ocaml" not in k[0]]
-    # CONFIGS = [k for k in COLORS if "Rupicola" in k[1]]
-    # CONFIGS = [k for k in COLORS if "C" in k[1]]
-    plot = add_plot(c_data, CONFIGS, ax=fig.add_subplot(111),
+    colors = {k: v for (k, v) in COLORS.items() if "ocaml" not in k[0]}
+    plot = add_plot(c_data, colors, ax=fig.add_subplot(111),
                     xlabel="Cycles per byte on 1MiB input (lower is better)")
     plot.legend(title="", loc='center right', bbox_to_anchor=(0.975, 0.425))
 
@@ -129,23 +129,17 @@ def ocaml_benchmarks():
         return
     fig = matplotlib.pyplot.figure(figsize=(8, 7))
 
-    CONFIGS = [
-        ('GCC 11.1', 'Rupicola'),
-        ('Clang 13.0', 'Rupicola'),
-
-        ('GCC 11.1', 'C'),
-        ('Clang 13.0', 'C'),
-
-        ('ocamlopt 4.09', 'Coq/OCaml'),
-    ]
-    p1 = add_plot(ocaml_data, CONFIGS, selected_benchmarks=['ip'],
+    COMPILERS = 'GCC 11.1', 'Clang 13.0', 'ocamlopt 4.09'
+    colors = {k: v for (k, v) in COLORS.items() if k[0] in COMPILERS}
+    p1 = add_plot(ocaml_data, colors, selected_benchmarks=['ip'],
                   ax=fig.add_subplot(2, 1, 1), logmin=False,
                   xlabel="Cycles per byte on 1MiB input, linear scale (lower is better)")
-    p2 = add_plot(ocaml_data, CONFIGS, selected_benchmarks=['ip'],
-                  ax=fig.add_subplot(2, 1, 2), logmin=1,
-                  xlabel="Cycles per byte on 1MiB input, log scale (lower is better)")
     p1.legend(title="", loc='upper right')
-    p2.legend(title="", loc='upper right')
+    # Uncomment to get a log plot; you may need to adjust logmin below
+    # p2 = add_plot(ocaml_data, colors, selected_benchmarks=['ip'],
+    #               ax=fig.add_subplot(2, 1, 2), logmin=1,
+    #               xlabel="Cycles per byte on 1MiB input, log scale (lower is better)")
+    # p2.legend(title="", loc='upper right')
 
     fig.tight_layout()
     fig.savefig("benchmark-ocaml.pdf")
